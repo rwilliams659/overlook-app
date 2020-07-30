3c8b43d5c1983ff0badc2179dbcba0a5ed518f5c
Rachel Williams <rachelwilliams659@gmail.com>
2020-07-30 11:46:34 -0600

    Style header

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import './images/reservation.png'
 // import domUpdates from './domUpdates';
 import UserRepo from './UserRepo';
 import RoomRepo from './RoomRepo';
