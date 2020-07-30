21e35a1c74cfb3113fac49e215440e3c0a01975e
Rachel Williams <rachelwilliams659@gmail.com>
2020-07-30 10:35:22 -0600

    Add properties for Room classes

--- src/Room.js
+++ src/Room.js
@@ -1,6 +1,11 @@
 class Room {
-  constructor() {
-
+  constructor(room) {
+    this.number = room.number;
+    this.roomType = room.roomType;
+    this.bidet = room.bidet;
+    this.bedSize = room.bedSize;
+    this.numBeds = room.numBeds;
+    this.costPerNight = room.costPerNight
   }
 }
 
--- src/RoomRepo.js
+++ src/RoomRepo.js
@@ -1,8 +1,8 @@
 import Room from './Room'
 
-class RoomRepo() {
-  constructor() {
-
+class RoomRepo {
+  constructor(rooms) {
+    this.rooms = rooms.map(room => new Room(room))
   }
 }
 
