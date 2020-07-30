f542a49b4425ce01ac68f265a9e46753373fc681
Rachel Williams <rachelwilliams659@gmail.com>
2020-07-30 10:15:47 -0600

    Create function to fetch all data from API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
 
@@ -7,4 +8,20 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+let userRepo, roomRepo, bookingRepo, currentUser, today
+
+window.onload = fetchData
+
+function fetchData() {
+  Promise.all([
+    fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users'),
+    fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms'),
+    fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
+  ])
+    .then(responses => Promise.all(responses.map(response => response.json())))
+    .then(([users, rooms, bookings]) => instantiateData(users, rooms, bookings))
+    .catch(error => console.log(error.message))
+}
+
+function instantiateData(users, rooms, bookings) {
+}
