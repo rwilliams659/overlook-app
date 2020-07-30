6d6b1b0b020848cfc35afb4d3022b57f89f00d00
Rachel Williams <rachelwilliams659@gmail.com>
2020-07-30 10:37:45 -0600

    Add properties for User classes

--- src/User.js
+++ src/User.js
@@ -1,6 +1,7 @@
 class User {
-  constructor() {
-
+  constructor(user) {
+    this.id = user.id; 
+    this.name = user.name;
   }
 }
 
--- src/UserRepo.js
+++ src/UserRepo.js
@@ -1,8 +1,8 @@
 import User from './User'
 
-class UserRepo() {
-  constructor() {
-
+class UserRepo {
+  constructor(users) {
+    this.users = users.map(user => new User(user)); 
   }
 }
 
