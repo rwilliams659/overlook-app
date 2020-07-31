0a9bef45beb29f16e6b2d79900b123754df506e0
Rachel Williams <rachelwilliams659@gmail.com>
2020-07-31 10:30:33 -0600

    Complete UserRepo class and tests

--- src/UserRepo.js
+++ src/UserRepo.js
@@ -4,6 +4,10 @@ class UserRepo {
   constructor(users) {
     this.users = users.map(user => new User(user)); 
   }
+
+  findUser(searchTerm) {
+    return this.users.find(user => user.name.includes(searchTerm));
+  }
 }
 
 export default UserRepo; 
--- test/UserRepo-test.js
+++ test/UserRepo-test.js
@@ -3,12 +3,14 @@ import UserRepo from '../src/UserRepo';
 import User from '../src/User';
 
 describe.only('UserRepo', function() {
-  let user1, user2, userRepo;
+  let user1, user2, user3, user4, userRepo;
 
   before(function() {
     user1 = { id: 10, name: "Taylor Swift" };
     user2 = { id: 17, name: 'Billie Eilish' };
-    userRepo = new UserRepo([user1, user2]);
+    user3 = { id: 24, name: 'Hayley Williams'}
+    user4 = { id: 3, name: 'Billie Joel' };
+    userRepo = new UserRepo([user1, user2, user3]);
   });
 
   it('should be a function', function() {
@@ -21,11 +23,21 @@ describe.only('UserRepo', function() {
 
   it('should store users', function() {
     expect(userRepo.users[1].id).to.equal(17)
-  })
-
-  it('should store instances of User', function() {
-    expect(userRepo.users[0]).to.be.an.instanceof(User);
   });
 
+  it('should store instances of User', function() {
+    expect(userRepo.users[2]).to.be.an.instanceof(User);
+  });
 
-})
+  it('given a full name, it should be able to return the user with that name', function() {
+    const getUser = userRepo.findUser('Hayley Williams');
+
+    expect(getUser).to.deep.equal({ id: 24, name: 'Hayley Williams' });
+  });
+
+  it('given a partial name, it should still be able to return the first user with that name', function() {
+    const getUser = userRepo.findUser('Billie');
+
+    expect(getUser).to.deep.equal({ id: 17, name: 'Billie Eilish' });
+  });
+});
